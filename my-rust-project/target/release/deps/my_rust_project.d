ff6e97f364aa723a59eb304089ddfd3dad7b9618
Kemet Dugue <kemet.dugue@gmail.com>
2021-10-08 20:09:40 -0700

    create rust in motion directory

--- my-rust-project/target/release/deps/my_rust_project.d
+++
@@ -1,5 +0,0 @@
-/Users/KemetDugue/code/repos/my-rust-project/target/release/deps/my_rust_project: src/main.rs
-
-/Users/KemetDugue/code/repos/my-rust-project/target/release/deps/my_rust_project.d: src/main.rs
-
-src/main.rs:
--- my-rust-project/target/release/my-rust-project.d
+++
@@ -1,1 +0,0 @@
-/Users/KemetDugue/code/repos/my-rust-project/target/release/my-rust-project: /Users/KemetDugue/code/repos/my-rust-project/src/main.rs
