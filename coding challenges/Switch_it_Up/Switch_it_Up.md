4b64a604aa132b069f3d881ec3a67d1629caad34
Kemet Dugue <kemet.dugue@gmail.com>
2021-11-21 09:49:43 -0800

    add switch it up solution

---
+++ coding challenges/Switch_it_Up/Switch_it_Up.md
@@ -0,0 +1,9 @@
+## Switch It Up!
+
+[Link](https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/rust)
+
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
---
+++ coding challenges/Switch_it_Up/Switch_it_Up.rs
@@ -0,0 +1,14 @@
+fn switch_it_up(n: usize) -> &'static str {
+    match n {
+        0 => "Zero",
+        1 => "One",
+        2 => "Two",
+        3 => "Three",
+        4 => "Four",
+        5 => "Five",
+        6 => "Six",
+        7 => "Seven",
+        8 => "Eight",
+        _ => "Nine",
+    }
+}
